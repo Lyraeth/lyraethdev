870972e26b10ec5fc9ac5c71d635c12e7499d28f
Mahsa Nurfarhan Hidayat <nurfarhan52@gmail.com>
2025-03-03 07:24:15 +0700

    feat (project/page.tsx): create Project Page

---
+++ app/project/page.tsx
@@ -0,0 +1,7 @@
+export default function Project() {
+    return (
+        <section className="max-w-screen-md mx-auto max-sm:px-4 items-center min-h-screen py-8">
+            <h1>Project</h1>
+        </section>
+    );
+}
